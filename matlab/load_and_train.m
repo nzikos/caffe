045889e3d106d18569672542b6d3b42b342b67ed
alexis78 <alexis78@users.noreply.github.com>
2015-10-21 21:56:12 +0300

    "testing matlab commit"

--- matlab/load_and_train.m
+++ matlab/load_and_train.m
@@ -3,3 +3,5 @@ net.load_snapshot(fullfile('/','media','alexis','SSD','CACHE','snapshots','snaps
 %net.set_display(1);                            %Display training stats every x iterations
 %net.set_snapshot_time(120);                       %Save a snapshot of the net every (time in minutes)
 net.start();
+
+%Testing matlab git commit
